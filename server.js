6b7eaeb2f6c175c7b127c50e29af081309ecc978
Mario Roque <A01282301@itesm.mx>
2020-05-11 00:19:15 -0500

    Generic get

--- server.js
+++ server.js
@@ -1,5 +1,38 @@
 const express = require('express');
 const app = express();
 
+//Data of students
+let listOfStudents = [
+    {
+        name: "Marcel",
+        id: "A0128200"
+    },
+    {
+        name: "Greg",
+        id: "A01282393"
+    },
+    {
+        name: "Jose",
+        id: "A02282393"
+    },
+    {
+        name: "Valerio",
+        id: "A11111111"
+    }
+]
 
 
+
+//Generic get Endpoint 
+app.get('/api/students', (req, res) =>{
+    console.log('getting all students')
+    return res.status(200).json(listOfStudents);
+} )
+
+//Port configuration
+app.listen( 8080, ()=>{
+    console.log("This app is running on the http port")
+
+})
+
+// webpage to be run on:   http://localhost:8080
